371a5e2437d1a8234c27533af0bd9b5b2d897397
Carolina Vejar <caro.vejarc@gmail.com>
2023-03-26 23:21:21 -0300

    Aplicando princicio de Liskov

--- src/solid/03-liskov-b.ts
+++ src/solid/03-liskov-b.ts
@@ -1,35 +1,62 @@
-export class Tesla {
+export abstract class Vehicle {
+    // getNumberOfSeat() : number {
+    //     throw new Error('method not implemented')
+    // }
 
-    constructor( private numberOfSeats: number ) {}
+    abstract getNumberOfSeats() : number;
+}
 
-    getNumberOfTeslaSeats() {
+export class Tesla extends Vehicle {
+
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
+
+    getNumberOfSeats() {
         return this.numberOfSeats;
     }
 }
 
-export class Audi {
+export class Audi extends Vehicle {
 
-    constructor( private numberOfSeats: number ) {}
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
 
-    getNumberOfAudiSeats() {
+    getNumberOfSeats() {
         return this.numberOfSeats;
     }
 }
 
-export class Toyota {
+export class Toyota extends Vehicle {
 
-    constructor( private numberOfSeats: number ) {}
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
 
-    getNumberOfToyotaSeats() {
+    getNumberOfSeats() {
         return this.numberOfSeats;
     }
 }
 
-export class Honda {
+export class Honda extends Vehicle {
 
-    constructor( private numberOfSeats: number ) {}
+    constructor( private numberOfSeats: number ) {
+        super()
+    }
 
-    getNumberOfHondaSeats() {
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
+
+export class Ford extends Vehicle {
+
+    constructor( private numberOfSeats: number ) {
+        super()
+    }
+    
+    getNumberOfSeats() {
         return this.numberOfSeats;
     }
 }
