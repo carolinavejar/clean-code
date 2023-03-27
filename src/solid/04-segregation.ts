3357218e5fa8d4f39333dcabfcc62fb1983e17bb
Carolina Vejar <caro.vejarc@gmail.com>
2023-03-26 23:34:32 -0300

    Creacion archivo Segregation

---
+++ src/solid/04-segregation.ts
@@ -0,0 +1,34 @@
+interface Bird {
+    fly(): void;
+    eat(): void;
+    run():void;
+    swim():void;
+}
+
+class Tucan implements Bird {
+    public fly()  { }
+    public eat()  { }
+    public run()  { }
+    public swim() { }
+}
+
+class Humminbird  implements Bird {
+    public fly()  { }
+    public eat()  { }
+    public run()  { }
+    public swim() { }
+}
+
+class Ostrich  implements Bird {
+    public fly()  { throw new Error ('Esta ave no vuela') }
+    public eat()  { }
+    public run()  { }
+    public swim() { }
+}
+
+class Penguin  implements Bird {
+    public fly()  { throw new Error ('Esta ave no vuela') }
+    public eat()  { }
+    public run()  { }
+    public swim() { }
+}
